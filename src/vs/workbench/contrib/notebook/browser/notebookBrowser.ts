f3bf8950c6b8f20e60e35034ef7ea92386ea21a7
rebornix <penn.lv@gmail.com>
2021-01-22 10:31:37 -0800

    style polish

--- src/vs/workbench/contrib/notebook/browser/notebookBrowser.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookBrowser.ts
@@ -686,6 +686,7 @@ export interface BaseCellRenderTemplate {
 export interface MarkdownCellRenderTemplate extends BaseCellRenderTemplate {
 	editorContainer: HTMLElement;
 	foldingIndicator: HTMLElement;
+	focusIndicatorBottom: HTMLElement;
 	currentEditor?: ICodeEditor;
 }
 
